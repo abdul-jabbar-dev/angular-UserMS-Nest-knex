848b851ac0a1b300c2a2c497fb5f9481cfec6296
abdul-jabbar-dev <abdul.jabbar.dev@gmail.com>
2024-08-09 16:15:47 +0600

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,39 @@
-import { NestFactory } from '@nestjs/core'; 
-import { AppModule } from './app.module';
-import * as cookie from 'cookie-parser'
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import * as cookie from "cookie-parser";
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(cookie())
- app.enableCors({
-   origin: "*",
-   methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
-   allowedHeaders: "Content-Type, Accept, Authorization",
- });
+
+  app.use(cookie());
+  app.use((req, res, next) => {
+    res.header(
+      "Access-Control-Allow-Origin",
+      "https://secondhand-item.netlify.app"
+    );
+    res.header(
+      "Access-Control-Allow-Methods",
+      "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS"
+    );
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Content-Type, Accept, Authorization"
+    );
+    res.header("Access-Control-Allow-Credentials", "true");
+    if (req.method === "OPTIONS") {
+      res.sendStatus(200);
+    } else {
+      next();
+    }
+  });
+
+  app.enableCors({
+    origin: ["https://secondhand-item.netlify.app"], 
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+    allowedHeaders: "Content-Type, Accept, Authorization",
+    credentials: true, 
+  });
+
   await app.listen(3000);
 }
 bootstrap();
