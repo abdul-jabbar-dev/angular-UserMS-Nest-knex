881047636cb5796b3b25243cfaf9f8f7ab244254
abdul-jabbar-dev <abdul.jabbar.dev@gmail.com>
2024-08-09 17:27:16 +0600

    cors

--- src/main.ts
+++ src/main.ts
@@ -1,35 +1,19 @@
-import { NestFactory } from "@nestjs/core";
-import { AppModule } from "./app.module";
-import * as cookie from "cookie-parser";
-import cors from "cors";
+import { NestFactory } from '@nestjs/core'; 
+import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  app.use(cookie());
-  app.use(
-    cors({
-      origin: (origin, callback) => {
-        const allowedOrigins = [
-          "https://secondhand-item.netlify.app",
-          "https://secondhand-item.netlify.app/login",
-          "https://secondhand-item.netlify.app/create",
-          "https://secondhand-item.netlify.app/*",
-          "http://localhost:4200",
-          origin,
-          "*",
-        ];
-        if (allowedOrigins.includes(origin)) {
-          callback(null, true);
-        } else {
-          callback(new Error("Not allowed by CORS"));
-        }
-      },
-      methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
-      allowedHeaders: "Content-Type, Accept, Authorization",
-    })
-  );
-
+  
+  app.use(cookieParser());
+  
+  app.enableCors({
+    origin: "https://secondhand-item.netlify.app", 
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+    allowedHeaders: "Content-Type, Accept, Authorization",
+    credentials: true,  
+  });
+  
   await app.listen(3000);
 }
 bootstrap();
