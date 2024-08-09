da53ef31ab3e9ee9d7efd3efe721a5510189218a
abdul-jabbar-dev <abdul.jabbar.dev@gmail.com>
2024-08-09 20:08:24 +0600

    working cors

--- dist/src/main.js
+++ dist/src/main.js
@@ -3,16 +3,18 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     app.use(cookieParser());
-    app.enableCors({
-        origin: "https://secondhand-item.netlify.app",
-        methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
-        allowedHeaders: "Content-Type, Accept, Authorization",
-        credentials: true,
-    });
-    await app.listen(3000);
+    app.enableCors();
+    app.use(cors({
+        origin: (origin, callback) => {
+            console.log("Request origin:", origin);
+            callback(null, true);
+        },
+    }));
+    await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,29 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import * as cookieParser from 'cookie-parser';
-
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import * as cookieParser from "cookie-parser";
+import * as cors from "cors";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.use(cookieParser());
 
-  app.enableCors({
-    origin: "*",
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS", 
-    allowedHeaders: "Content-Type, Accept, Authorization",
-    credentials: true, 
-  });
+  app.enableCors();
+  app.use(
+    cors({
+      origin: (origin, callback) => { 
+        console.log("Request origin:", origin);
+ 
+        callback(null, true);
+      },
+    })
+  );
+  // app.enableCors({
+  //   origin: "*",
+  //   methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+  //   allowedHeaders: "Content-Type, Accept, Authorization",
+  //   credentials: true,
+  // });
 
-  await app.listen(process.env.PORT||3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
